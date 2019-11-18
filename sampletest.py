ffedfc83487303a12bc9b4921b0b248746a86f86
Balachander B <balachanderb@ITs-MacBook-Pro-3.local>
2019-11-18 16:39:09 +0530

    first commit

---
+++ sampletest.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+user_name = "vonceil@uetimer.com"
+password = "upperandlowercaseletters"
+driver = webdriver.Chrome()
+driver.get('https://bugcrowd:wow wobbly whiplash barbecue@redama.com/user/sign_in')
+element = driver.find_element_by_id("user_email")
+element.send_keys(user_name)
+element = driver.find_element_by_id("user_password")
+element.send_keys(password)
+element.send_keys(Keys.RETURN)
+element.close()
+driver.close()
+driver.quit()
